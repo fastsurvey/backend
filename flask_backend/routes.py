b061ddfd65ed8edf17c726151ff46ccf21d21139
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-05-04 01:20:53 +0200

    Refactor routes for multiple surveys

--- flask_backend/routes.py
+++ flask_backend/routes.py
@@ -1,23 +1,32 @@
 
-from flask_backend import app
-from flask_backend.database import submit, verify
-from flask import render_template, request, redirect
+from flask_backend import app, FRONTEND_URL
+from flask_backend.surveys.survey_1 import survey_1_actions
+
+from flask_backend.support_functions import formatting
+
+from flask import request, redirect
 
 
-@app.route("/backend/submit", methods=["POST"])
-def backend_submit_form_data():
+@app.route("/submit/<survey_date>", methods=["POST"])
+def backend_submit_form_data(survey_date):
+
+    if survey_date == "20200505":
+        submit = survey_1_actions.submit
+    else:
+        return formatting.status("invalid survey")
+
     result = submit(request.get_json(force=True))
     print(result)
     return {"status": result["status"]}, result["status_code"]
 
 
-@app.route("/backend/verify/<verification_token>", methods=["GET"])
-def backend_verify_form_data(verification_token):
+@app.route("/verify/<survey_date>/<verification_token>", methods=["GET"])
+def backend_verify_form_data(survey_date, verification_token):
+
+    if survey_date == "20200505":
+        verify = survey_1_actions.verify
+    else:
+        return formatting.status("invalid survey")
+
     verify(verification_token)
-    return redirect("/success")
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    # All the remaining routes are routed to the frontend production build
-    return render_template("index.html")
+    return redirect(f"{FRONTEND_URL}{survey_date}/success")
