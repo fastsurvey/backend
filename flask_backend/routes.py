a520bc6c78f5a6e6dc19217e9be8f29a0553cacd
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-05-05 22:53:16 +0200

    Add routes for fvv-ss20-referate #3

--- flask_backend/routes.py
+++ flask_backend/routes.py
@@ -1,6 +1,7 @@
 
 from flask_backend import app, FRONTEND_URL, pending_entries_collection, verified_entries_collection, time_limits_collection
 from flask_backend.surveys.survey_1 import survey_1_actions, survey_1_results
+from flask_backend.surveys.survey_2 import survey_2_actions, survey_2_results
 
 from flask_backend.support_functions import formatting
 
@@ -12,24 +13,24 @@ def backend_status():
     try:
         pending_entries_collection.count_documents()
         verified_entries_collection.count_documents()
-
         # TODO: Add test for sending mails
-
         return {"status": "all services operational"}, 200
     except:
         return {"status": "database error"}, 200
 
 
-@app.route("/<survey_date>/submit", methods=["POST"])
-def backend_submit(survey_date):
+@app.route("/<survey_name>/submit", methods=["POST"])
+def backend_submit(survey_name):
 
-    if survey_date == "20200504":
+    if survey_name == "20200504":
         submit = survey_1_actions.submit
+    if survey_name == "fvv-ss20-referate":
+        submit = survey_2_actions.submit
     else:
         return formatting.status("survey invalid"), 400
 
     # Checking whether the survey is currently open
-    time_limit_record = time_limits_collection.find_one({"survey_name": survey_date})
+    time_limit_record = time_limits_collection.find_one({"survey_name": survey_name})
     if time_limit_record is not None:
         if not time_limit_record["is_active"]:
             # Only when one has specifically set the survey
@@ -44,22 +45,28 @@ def backend_submit(survey_date):
     return {"status": result["status"]}, result["status_code"]
 
 
-@app.route("/<survey_date>/verify/<verification_token>", methods=["GET"])
-def backend_verify(survey_date, verification_token):
+@app.route("/<survey_name>/verify/<verification_token>", methods=["GET"])
+def backend_verify(survey_name, verification_token):
 
-    if survey_date == "20200504":
+    if survey_name == "20200504":
         verify = survey_1_actions.verify
-    else:
-        return formatting.status("survey invalid")
-
-    verify(verification_token)
-    return redirect(f"{FRONTEND_URL}{survey_date}/success")
-
-
-@app.route("/<survey_date>/results", methods=["GET"])
-def backend_results(survey_date):
-
-    if survey_date == "20200504":
-        return survey_1_results.fetch()
+    elif survey_name == "fvv-ss20-referate":
+        verify = survey_2_actions.verify
     else:
         return formatting.status("survey invalid"), 400
+
+    verify(verification_token)
+    return redirect(f"{FRONTEND_URL}{survey_name}/success")
+
+
+@app.route("/<survey_name>/results", methods=["GET"])
+def backend_results(survey_name):
+
+    if survey_name == "20200504":
+        fetch = survey_1_results.fetch
+    if survey_name == "fvv-ss20-referate":
+        fetch = survey_2_results.fetch
+    else:
+        return formatting.status("survey invalid"), 400
+
+    return fetch()
