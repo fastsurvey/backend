cf7c83c5a832cf001770f79c8cace42922a46107
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-05-06 04:03:12 +0200

    Remove useless code

--- flask_backend/surveys/survey_3/survey_3_results.py
+++ flask_backend/surveys/survey_3/survey_3_results.py
@@ -5,19 +5,13 @@ from flask_backend.support_functions import formatting
 def fetch():
     verified_records = list(verified_entries_collection.find({"survey": "fvv-ss20-go"}))
 
-    # Did not check for different email capitalizations in this survey yet
-    # Back then "ge69zeh" and "GE69zeh" could vote twice
-    # Here: Manual fix (faster)
-    voter_emails = []
-
     options = ["ja", "nein", "enthaltung"]
     results = {}
     for option in options:
         results[option] = 0
 
     for record in verified_records:
-        if record["email"] not in voter_emails:
-            for option in options:
-                results[option] += 1 if record["election"][option] else 0
+        for option in options:
+            results[option] += 1 if record["election"][option] else 0
 
     return formatting.status("ok", results=results)
