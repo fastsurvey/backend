b39c247afe945512768095645835494cbe721bfa
Felix Böhm <felix.boehm@tum.de>
2020-05-10 15:47:52 +0200

    Adapt verify function from surveys to fastapi and survey class model #18

--- app/survey.py
+++ app/survey.py
@@ -1,23 +1,54 @@
 from abc import ABC, abstractmethod
+from pymongo import DeleteMany, InsertOne
+
 
 class Survey(ABC):
     
     def __init__(
             self,
             identifier,
+            description,
+            database,
     ):
-        self.identifier = identifier
+        self.id = identifier
+        self.description = description
+        self.db = database
 
     @abstractmethod
-    def submit(self):
+    async def validate(self, submission):
+        """Validate the correct format of a user submission."""
         pass
 
     @abstractmethod
-    def verify(self, token):
+    async def submit(self, submission):
+        """Receive a user submission and save it in the pending entries
+        collection to be verified.
+
+        """
         pass
 
+    async def verify(self, token):
+        """Verify user submission by searching the corresponding user entry in 
+        the pending entries collection and moving it to verified entries.
+
+        """
+        pending = await self.db['pending_entries'].find_one({
+            'verification_token': token,
+            'survey': self.id,
+        })
+        if pending is not None:
+            del pending['verification_token']
+            requests = [
+                DeleteMany({
+                    'email': pending['email'], 
+                    'survey': self.id,
+                }),
+                InsertOne(pending),
+            ]
+            await self.db['verified_entries'].bulk_write(requests, ordered=True)
+
     @abstractmethod
-    def results(self):
+    async def results(self):
         pass
 
 
@@ -26,21 +57,18 @@ class SingleChoiceSurvey(Survey):
     def __init__(
             self,
             identifier,
+            description,
+            database,
             choices,
     ):
-        Survey.__init__(self, identifier)
+        Survey.__init__(self, identifier, description, database)
         self.choices = choices
+    
+    async def validate(self):
+        print('Submission validated!')
 
-    def submit(self):
-        print('Choice submitted!')
+    async def submit(self):
+        print('Submission received successfully!')
 
-    def verify(self, token):
-        print('Token verified!')
-
-    def results(self):
+    async def results(self):
         print(f'Results: {self.choices[0]}: 60% vs. {self.choices[1]}: 40%!')
-
-
-if __name__ == "__main__":
-    survey = SingleChoiceSurvey('first survey', ['A', 'B'])
-    survey.results()
