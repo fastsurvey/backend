5587c8e2be98263b22fcec78246c9f6a6b5710fa
Felix Böhm <felix.boehm@tum.de>
2020-05-31 20:57:46 +0200

    Integrate email lists into respective functions and copy valid submission template before changing its email

--- app/tests/test_validation.py
+++ app/tests/test_validation.py
@@ -54,75 +54,59 @@ def test_generate_schema(template):
     }
 
 
-pass_submissions = [
-    {
-        'email': 'tt00est@mytum.de',
-        'properties': {
-            'election': {
-                'felix': True,
-                'moritz': True,
-                'andere': '',
-            },
-            'reason': 'hello world!',
-        }
-    },
-    {
-        'email': 'tt00est@mytum.de',
-        'properties': {
-            'election': {
-                'felix': False,
-                'moritz': False,
-                'andere': 'Max Mustermann',
-            },
-            'reason': 'foooooooooooo baaaaaaaaaaaaaaar',
-        }
-    },
-]
+submission = {
+    'email': 'tt00est@mytum.de',
+    'properties': {
+        'election': {
+            'felix': True,
+            'moritz': True,
+            'andere': '',
+        },
+        'reason': 'hello world!',
+    }
+}
 
 
 def test_validator_passing(validator):
-    """Check that the generated validator lets some valid submissions pass."""
-    for submission in pass_submissions:
-        assert validator.validate(submission)
+    """Check that the generated validator lets a valid submissions pass."""
+    assert validator.validate(submission)
 
-
-pass_emails = [
-    'tt00est@mytum.de',
-    'ia72ihd@mytum.de',
-]
-
-fail_emails = [
-    8,
-    None,
-    {},
-    True,
-    '',
-    'sadfj',
-    'FFFFFFF@mytum.de',
-    'tt00est@mytum.de ',
-    'a123adf@mytum.de',
-    'ab82eee@mytum8de',
-    'a12 93ad@mytum.de',
-    'a123   @mytum.de',
-    'a444+00@mytum.de',
-    'tt00est@gmail.com',
-    '123@mytum.de@mytum.de',
-    'tt00est@mytum:de',
-    'tT00est@mytum.de',
-    'tt00eSt@mytum.de',
-]
 
 def test_email_passing(validator):
     """Check that the validator lets some valid email addresses pass."""
-    for email in pass_emails:
-        submission = pass_submissions[0]
-        submission['email'] = email
-        assert validator.validate(submission)
+    emails = [
+        'tt00est@mytum.de',
+        'ia72ihd@mytum.de',
+    ]
+    sub = submission.copy()
+    for email in emails:
+        sub['email'] = email
+        assert validator.validate(sub)
 
 
 def test_email_failing(validator):
     """Check that the validator rejects some invalid email addresses."""
-    for email in fail_emails:
-        submission = pass_submissions[0]
-        submission['email'] = email
-        assert not validator.validate(submission)
+    emails = [
+        8,
+        None,
+        {},
+        True,
+        '',
+        'sadfj',
+        'FFFFFFF@mytum.de',
+        'tt00est@mytum.de ',
+        'a123adf@mytum.de',
+        'ab82eee@mytum8de',
+        'a12 93ad@mytum.de',
+        'a123   @mytum.de',
+        'a444+00@mytum.de',
+        'tt00est@gmail.com',
+        '123@mytum.de@mytum.de',
+        'tt00est@mytum:de',
+        'tT00est@mytum.de',
+        'tt00eSt@mytum.de',
+    ]
+    sub = submission.copy()
+    for email in emails:
+        sub['email'] = email
+        assert not validator.validate(sub)
