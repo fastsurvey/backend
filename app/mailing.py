5d26fa8c4729a7859472fb048fbe831d943da425
Felix Böhm <felix@felixboehm.dev>
2020-11-18 13:06:25 +0100

    Copy and adapt email sending functions related to account management from auth backend

--- app/mailing.py
+++ app/mailing.py
@@ -35,7 +35,7 @@ class Letterbox:
         response = await self.client.post('/messages', data=data)
         return response.status_code
 
-    async def send_verification_email(
+    async def send_submission_verification_email(
             self,
             admin_name,
             survey_name,
@@ -43,14 +43,39 @@ class Letterbox:
             receiver,
             token,
         ):
-        """Send confirmation email in order to verify an email address."""
-        # verification url
+        """Send confirmation email to verify a submission email address."""
         subject = 'Please verify your submission'
+        # verification url
         vurl = f'{BURL}/{admin_name}/{survey_name}/verification/{token}'
         html = (
             '<p>Hi there, we received your submission!</p>'
             + f'<p>Survey: <strong>{title}</strong></p>'
             + f'<p>Please verify your submission by <a href="{vurl}" target="_blank">clicking here</a>.</p>'
+            + '<p>Best, the FastSurvey team</p>'
+        )
+        return await self.send(receiver, subject, html)
+
+    async def send_account_verification_email(self, receiver: str, token: str):
+        """Send confirmation email to verify an account email address."""
+        subject = 'Welcome to FastSurvey!'
+        # verification url
+        vurl = f'{FURL}/verify?token={token}'
+        html = (
+            '<p>Welcome to FastSurvey!</p>'
+            + f'<p>Please verify your email address by <a href="{vurl}" target="_blank">clicking here</a>.</p>'
+            + '<p>The verification link is valid for 10 minutes.</p>'
+            + '<p>Best, the FastSurvey team</p>'
+        )
+        return await self.send(receiver, subject, html)
+
+    async def send_password_reset_email(self, receiver: str, token: str):
+        """Send email in order to reset the password of an existing account."""
+        subject = 'Reset Your FastSurvey Password'
+        # password reset url
+        rurl = f'{FURL}/set-password?token={token}'
+        html = (
+            '<p>Hi there,</p>'
+            + f'<p>You can set your new password by <a href="{rurl}" target="_blank">clicking here</a>.</p>'
             + '<p>Best, the FastSurvey team</p>'
         )
         return await self.send(receiver, subject, html)
