d9e4b01a37860e62589527f7002fbe0b7afa752a
Felix Böhm <felix@felixboehm.dev>
2020-11-22 02:48:53 +0100

    Add function to create OAuth2 tokens

--- app/cryptography.py
+++ app/cryptography.py
@@ -25,11 +25,11 @@ class PasswordManager:
             deprecated='auto',
         )
 
-    def hash_password(self, password: str):
+    def hash_password(self, password):
         """Hash the given password and return the hash as string."""
         return self.context.hash(password)
 
-    def verify_password(self, password: str, pwdhash: str):
+    def verify_password(self, password, pwdhash):
         """Return true if the password results in the hash, else False."""
         return self.context.verify(password, pwdhash)
 
@@ -38,28 +38,20 @@ class TokenManager:
     """The TokenManager manages encoding and decoding JSON Web Tokens."""
 
     ACCESS_TOKEN_TTL = 30*60  # 30 minutes
-    REFRESH_TOKEN_TTL = 48*60*60  # 2 days
+    REFRESH_TOKEN_TTL = 7*24*60*60  # 7 days
 
-    def generate(self, user_id: str, time_to_live: int):
+    def generate(self, admin_id, time_to_live):
         """Generate a JWT containing the user id and an expiration date."""
         timestamp = now()
         payload = {
             'iss': 'FastSurvey',
-            'sub': user_id,
+            'sub': admin_id,
             'iat': timestamp,
             'exp': timestamp + time_to_live,
         }
         return jwt.encode(payload, PRIVATE_RSA_KEY, algorithm='RS256')
 
-    def generate_access_token(self, user_id: str):
-        """Generate an access token JWT containing the user id."""
-        return self.generate(user_id, self.ACCESS_TOKEN_TTL)
-
-    def generate_refresh_token(self, user_id: str):
-        """Generate a refresh token JWT containing the user id."""
-        return self.generate(user_id, self.REFRESH_TOKEN_TTL)
-
-    def decode(self, token: str):
+    def decode(self, token):
         """Decode the given JWT and return the user id."""
         try:
             payload = jwt.decode(token, PUBLIC_RSA_KEY, algorithm='RS256')
@@ -70,3 +62,11 @@ class TokenManager:
         except InvalidTokenError:
             raise HTTPException(400, 'invalid token format')
         return payload['sub']
+
+    def oauth(self, admin_id):
+        """Generate an OAuth2 token containing an access and refresh JWT."""
+        return {
+            'access_token': self.generate(admin_id, self.ACCESS_TOKEN_TTL),
+            'refresh_token': self.generate(admin_id, self.REFRESH_TOKEN_TTL),
+            'token_type': 'bearer',
+        }
