244bf406ef93b92bf162add5f5107dc372015d8c
Felix Böhm <felix@felixboehm.dev>
2021-05-31 23:20:04 +0200

    Move error messages into own module

---
+++ app/errors.py
@@ -0,0 +1,28 @@
+# 400
+INVALID_ACCOUNT_DATA = 'invalid account data'
+INVALID_CONFIGURATION = 'invalid configuration'
+USERNAME_ALREADY_TAKEN = 'username already taken'
+EMAIL_ADDRESS_ALREADY_TAKEN = 'email address already taken'
+ACCOUNT_NOT_VERIFIED = 'account not verified'
+ACCOUNT_ALREADY_VERIFIED = 'account already verified'
+SURVEY_EXISTS = 'survey exists'
+NOT_AN_EXISTING_SURVEY = 'not an existing survey'
+SURVEY_IS_NOT_OPEN_YET = 'survey is not open yet'
+SURVEY_IS_CLOSED = 'survey is closed'
+SURVEY_IS_NOT_YET_CLOSED = 'survey is not yet closed'
+INVALID_SUBMISSION = 'invalid submission'
+
+# 401
+INVALID_VERIFICATION_TOKEN = 'invalid verification token'
+INVALID_PASSWORD = 'invalid password'
+
+# 404
+USER_NOT_FOUND = 'user not found'
+SURVEY_NOT_FOUND = 'survey not found'
+
+# 500
+ACCOUNT_CREATION_ERROR = 'account creation error'
+EMAIL_DELIVERY_FAILURE = 'email delivery failure'
+
+# 501
+NOT_IMPLEMENTED = 'not implemented'
