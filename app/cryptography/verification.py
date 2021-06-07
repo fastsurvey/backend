e9e4361c92c8c67819cfa09971f9e0ce08270d28
Felix Böhm <felix@felixboehm.dev>
2021-06-07 18:57:16 +0200

    Shorten verification token by using token_urlsafe instead of token_hex

--- app/cryptography/verification.py
+++ app/cryptography/verification.py
@@ -2,5 +2,5 @@ import secrets
 
 
 def token():
-    """Create and return a random hex string useful in verification flows."""
-    return secrets.token_hex(64)
+    """Create and return a random string useful for verification flows."""
+    return secrets.token_urlsafe(48)
