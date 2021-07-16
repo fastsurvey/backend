7a5e7c3e765adc6943a937ed3b3160cf4df154b3
Felix Böhm <felix@felixboehm.dev>
2021-07-16 17:09:22 +0200

    Repair submission verification tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,4 @@
 import pytest
-import secrets
 import httpx
 import copy
 
@@ -492,7 +491,7 @@ async def test_creating_valid_submission(
         cleanup,
     ):
     """Test that submission works given a valid submission."""
-    response = await client.post(
+    await client.post(
         url=f'/users/{username}/surveys/{survey_name}',
         headers=headers,
         json=configuration,
@@ -518,7 +517,7 @@ async def test_creating_invalid_submission(
         cleanup,
     ):
     """Test that submit correctly fails given an invalid submissions."""
-    response = await client.post(
+    await client.post(
         url=f'/users/{username}/surveys/{survey_name}',
         headers=headers,
         json=configuration,
@@ -538,168 +537,112 @@ async def test_creating_invalid_submission(
 ################################################################################
 
 
-# TODO go through tests, don't skip any
-
-
-@pytest.mark.skip(reason='scheduled for refactoring')
 @pytest.mark.asyncio
-async def test_duplicate_validation_token_resolution(
-        monkeypatch,
+async def test_verifying_valid_verification_token(
+        mock_email_sending,
+        mock_verification_token_generation,
         client,
+        headers,
         username,
-        submissionss,
+        survey_name,
+        configuration,
+        submissions,
         cleanup,
     ):
-    """Test that duplicate tokens in submissions are correctly resolved."""
-    survey_name = 'complex-survey'
-    survey = await main.survey_manager.fetch(username, survey_name)
-    submissions = submissionss[survey_name]['valid']
-    tokens = []
-
-    def token(length):
-        """Return predefined tokens in order to test token validation."""
-        tokens.append(str(len(tokens) // 3))
-        return tokens[-1]
-
-    monkeypatch.setattr(secrets, 'token_hex', token)  # mock token generation
-
-    for i, submission in enumerate(submissions):
-        response = await client.post(
-            url=f'/users/{username}/surveys/{survey_name}/submissions',
-            json=submission,
-        )
-        assert response.status_code == 200
-        entry = await survey.submissions.find_one({'data': submission})
-        assert entry is not None
-        assert entry['_id'] == str(i)
-
-
-@pytest.mark.skip(reason='scheduled for refactoring')
-@pytest.mark.asyncio
-async def test_verifying_valid_token(
-        monkeypatch,
-        client,
-        username,
-        submissionss,
-        cleanup,
-    ):
-    """Test correct verification given a valid submission token."""
-    survey_name = 'complex-survey'
-    survey = await main.survey_manager.fetch(username, survey_name)
-    submissions = submissionss[survey_name]['valid']
-    tokens = []
-
-    def token(length):
-        """Return predefined tokens in order to test token validation."""
-        tokens.append(str(len(tokens)))
-        return tokens[-1]
-
-    monkeypatch.setattr(secrets, 'token_hex', token)  # token generation mock
-
-    base_url = f'/users/{username}/surveys/{survey_name}'
-    for submission in submissions:
-        await client.post(f'{base_url}/submission', json=submission)
-    for i, token in enumerate(tokens):
-        response = await client.get(
-            url=f'{base_url}/verification/{token}',
-            allow_redirects=False,
-        )
-        assert response.status_code == 307
-        entry = await survey.submissions.find_one({'_id': token})
-        ve = await survey.vss.find_one({'_id': f'test+{i}@fastsurvey.io'})
-        assert entry is not None  # still unchanged in submissions
-        assert ve is not None  # now also in verified submissions
-
-
-@pytest.fixture(scope='function')
-async def scenario2(survey):
-    """Load some predefined entries into the database for testing."""
-    await survey.pending.insert_many([
-        {
-            '_id': 'tomato',
-            'email': 'aa00aaa@mytum.de',
-            'timestamp': 1590228251,
-            'properties': {'1': 'cucumber'},
-        },
-        {
-            '_id': 'carrot',
-            'email': 'aa00aaa@mytum.de',
-            'timestamp': 1590228461,
-            'properties': {'1': 'salad'},
-        },
-    ])
-    await survey.verified.insert_many([
-        {
-            '_id': 'aa00aaa@mytum.de',
-            'timestamp': 1590228043,
-            'properties': {'1': 'radish'},
-        },
-        {
-            '_id': 'aa02aaa@mytum.de',
-            'timestamp': 1590228136,
-            'properties': {'1': 'cabbage'},
-        },
-    ])
-
-
-@pytest.mark.skip(reason='scheduled for refactoring')
-@pytest.mark.asyncio
-async def test_verifying_replace_valid_token(scenario2, survey):
-    """Test replacement of previously verified submission."""
-    token = 'tomato'
-    async with httpx.AsyncClient(app=main.app, base_url='http://test') as c:
-        response = await c.get(
-            url=f'/fastsurvey/test/verify/{token}',
-            allow_redirects=False,
-        )
-    pe = await survey.pending.find_one(
-        filter={'_id': 'tomato'},
+    """Test submission is correctly verified given valid verification token."""
+    path = f'/users/{username}/surveys/{survey_name}'
+    await client.post(url=path, headers=headers, json=configuration)
+    await client.post(url=f'{path}/submissions', json=submissions[0])
+    token = str(0)
+    response = await client.get(
+        url=f'{path}/verification/{token}',
+        allow_redirects=False,
     )
-    ve = await survey.verified.find_one(
-        filter={'_id': 'aa00aaa@mytum.de'},
-    )
-    keys = {'_id', 'timestamp', 'properties'}
     assert response.status_code == 307
-    assert pe is not None  # entry is still unchanged in pending entries
-    assert ve is not None  # entry replaces previously verified entry
-    assert set(ve.keys()) == keys
-    assert ve['properties']['1'] == 'cucumber'
+    survey = await main.survey_manager.fetch(username, survey_name)
+    e = await survey.unverified_submissions.find_one({'_id': token})
+    assert e is not None  # still unchanged in unverified submissions
+    e = await survey.submissions.find_one(
+        filter={'_id': submissions[0][str(1)]},
+    )
+    assert e is not None  # now also in valid submissions
 
 
-@pytest.mark.skip(reason='scheduled for refactoring')
 @pytest.mark.asyncio
-async def test_verifying_invalid_token(scenario2, survey):
-    """Test correct verification rejection given an invalid token."""
-    token = 'peach'
-    async with httpx.AsyncClient(app=main.app, base_url='http://test') as c:
-        response = await c.get(
-            url=f'/fastsurvey/test/verify/{token}',
-            allow_redirects=False,
-        )
-    pes = await survey.pending.find().to_list(10)
-    ve = await survey.verified.find_one(
-        filter={'_id': 'aa00aaa@mytum.de'},
+async def test_verifying_valid_verification_token_submission_replacement(
+        mock_email_sending,
+        mock_verification_token_generation,
+        client,
+        headers,
+        username,
+        survey_name,
+        configuration,
+        submissions,
+        cleanup,
+    ):
+    """Test that previously verified submission is replaced with a new one."""
+    path = f'/users/{username}/surveys/{survey_name}'
+    await client.post(url=path, headers=headers, json=configuration)
+    # push first submission
+    submission = submissions[0]
+    email_address = submission[str(1)]
+    await client.post(url=f'{path}/submissions', json=submission)
+    await client.get(url=f'{path}/verification/0', allow_redirects=False)
+    # push second submission with the same email address
+    submission = copy.deepcopy(submissions[1])
+    submission[str(1)] = email_address
+    await client.post(url=f'{path}/submissions', json=submission)
+    await client.get(url=f'{path}/verification/1', allow_redirects=False)
+    survey = await main.survey_manager.fetch(username, survey_name)
+    x = await survey.submissions.find_one({'_id': email_address})
+    assert x['data'] == submission
+
+
+@pytest.mark.asyncio
+async def test_verifying_invalid_verification_token(
+        mock_email_sending,
+        mock_verification_token_generation,
+        client,
+        headers,
+        username,
+        survey_name,
+        configuration,
+        cleanup,
+    ):
+    """Test that request is rejected given an invalid verification token."""
+    path = f'/users/{username}/surveys/{survey_name}'
+    await client.post(url=path, headers=headers, json=configuration)
+    response = await client.get(
+        url=f'{path}/verification/token',
+        allow_redirects=False,
     )
     assert response.status_code == 401
-    assert len(pes) == 2  # entries are unchanged in pending entries
-    assert ve is not None  # old entry is still present
-    assert ve['properties']['1'] == 'radish'
+    survey = await main.survey_manager.fetch(username, survey_name)
+    x = await survey.submissions.find_one({})
+    assert x is None
 
 
-@pytest.mark.skip(reason='scheduled for refactoring')
 @pytest.mark.asyncio
-async def test_verifying_with_no_prior_submission(survey):
-    token = 'olive'
-    async with httpx.AsyncClient(app=main.app, base_url='http://test') as c:
-        response = await c.get(
-            url=f'/fastsurvey/test/verify/{token}',
-            allow_redirects=False,
-        )
-    pe = await survey.pending.find_one()
-    ve = await survey.verified.find_one()
-    assert response.status_code == 401
-    assert pe is None
-    assert ve is None
+async def test_duplicate_verification_token_resolution(
+        mock_email_sending,
+        mock_verification_token_generation_with_duplication,
+        client,
+        headers,
+        username,
+        survey_name,
+        configuration,
+        submissions,
+        cleanup,
+    ):
+    """Test submission is correctly verified given valid verification token."""
+    path = f'/users/{username}/surveys/{survey_name}'
+    await client.post(url=path, headers=headers, json=configuration)
+    for submission in submissions[:2]:
+        await client.post(url=f'{path}/submissions', json=submission)
+    survey = await main.survey_manager.fetch(username, survey_name)
+    for token in '01':
+        e = await survey.unverified_submissions.find_one({'_id': token})
+        assert e is not None
 
 
 ################################################################################
@@ -745,8 +688,6 @@ async def test_fetching_results_a(
 
 @pytest.mark.asyncio
 async def test_fetching_results_without_submissions(
-        mock_email_sending,
-        mock_verification_token_generation,
         client,
         headers,
         username,
