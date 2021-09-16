cc7293e6226a3cd0c806050550c58bc802f4fa19
Felix Böhm <felix@felixboehm.dev>
2021-09-16 19:05:24 +0200

    Ensure that there are no option duplicates in radio/selection fields; Close #71

--- app/models.py
+++ app/models.py
@@ -104,6 +104,12 @@ class RadioField(Field):
         max_items=Length.A,
     )
 
+    @pydantic.validator('options')
+    def validate_options(cls, v):
+        if len(set(v)) < len(v):
+            raise ValueError('options must be unique')
+        return v
+
 
 class SelectionField(Field):
     type: typing.Literal['selection']
@@ -118,6 +124,12 @@ class SelectionField(Field):
     )
     min_select: pydantic.conint(strict=True, ge=0)
     max_select: pydantic.conint(strict=True, ge=0)
+
+    @pydantic.validator('options')
+    def validate_options(cls, v):
+        if len(set(v)) < len(v):
+            raise ValueError('options must be unique')
+        return v
 
     @pydantic.validator('max_select')
     def validate_max_select(cls, v, values):
@@ -186,8 +198,8 @@ def validate_option_field_submission(cls, v):
 
 
 def validate_selection_field_submission(cls, v):
-    if len(set(v)) != len(v):
-        raise ValueError('no duplicates allowed')
+    if len(set(v)) < len(v):
+        raise ValueError('no duplicate selections allowed')
     return v
 
 
