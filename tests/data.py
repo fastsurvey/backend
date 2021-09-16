9a3dc7d7c0a85338983b6d02ce85289da152e3d1
Felix Böhm <felix@felixboehm.dev>
2021-09-16 19:04:34 +0200

    Add test data for invalid radio/selection/text field configurations; #71

--- tests/data.py
+++ tests/data.py
@@ -163,17 +163,91 @@ def _build_invalid_option_configurations(configuration):
 
 def _build_invalid_radio_configurations(configuration):
     """Build some invalid configurations for the radio test survey."""
-    return []
+    invalid_configurations = []
+    # options parameter has invalid type
+    x = copy.deepcopy(configuration)
+    x['fields'][0]['options'] = None
+    invalid_configurations.append(x)
+    # options parameter is missing
+    x = copy.deepcopy(configuration)
+    x['fields'][0].pop('options')
+    invalid_configurations.append(x)
+    # options are not unique
+    x = copy.deepcopy(configuration)
+    x['fields'][0]['options'] += x['fields'][0]['options'][0]
+    invalid_configurations.append(x)
+    # options list is empty
+    x = copy.deepcopy(configuration)
+    x['fields'][0]['options'] = []
+    invalid_configurations.append(x)
+
+    return invalid_configurations
 
 
 def _build_invalid_selection_configurations(configuration):
     """Build some invalid configurations for the selection test survey."""
-    return []
+    invalid_configurations = []
+    # options parameter has invalid type
+    x = copy.deepcopy(configuration)
+    x['fields'][0]['options'] = None
+    invalid_configurations.append(x)
+    # min_select parameter has invalid type
+    x = copy.deepcopy(configuration)
+    x['fields'][0]['min_select'] = float(x['fields'][0]['min_select'])
+    invalid_configurations.append(x)
+    # max_select parameter is missing
+    x = copy.deepcopy(configuration)
+    x['fields'][0].pop('max_select')
+    invalid_configurations.append(x)
+    # options are not unique
+    x = copy.deepcopy(configuration)
+    x['fields'][0]['options'] += x['fields'][0]['options'][0]
+    invalid_configurations.append(x)
+    # options list is empty
+    x = copy.deepcopy(configuration)
+    x['fields'][0]['options'] = []
+    invalid_configurations.append(x)
+    # min_select is greater than max_select
+    x = copy.deepcopy(configuration)
+    x['fields'][0]['min_select'] = x['fields'][0]['max_select'] + 1
+    invalid_configurations.append(x)
+    # min_select is less than zero
+    x = copy.deepcopy(configuration)
+    x['fields'][0]['min_select'] = -1
+    invalid_configurations.append(x)
+    # max_select is greater than number of options
+    x = copy.deepcopy(configuration)
+    x['fields'][0]['max_select'] = len(x['fields'][0]['options']) + 1
+    invalid_configurations.append(x)
+
+    return invalid_configurations
 
 
 def _build_invalid_text_configurations(configuration):
     """Build some invalid configurations for the text test survey."""
-    return []
+    invalid_configurations = []
+    # min_chars parameter has invalid type
+    x = copy.deepcopy(configuration)
+    x['fields'][0]['min_chars'] = float(x['fields'][0]['min_chars'])
+    invalid_configurations.append(x)
+    # max_chars parameter is missing
+    x = copy.deepcopy(configuration)
+    x['fields'][0].pop('max_chars')
+    invalid_configurations.append(x)
+    # min_chars is greater than max_chars
+    x = copy.deepcopy(configuration)
+    x['fields'][0]['min_chars'] = x['fields'][0]['max_chars'] + 1
+    invalid_configurations.append(x)
+    # min_chars is less than zero
+    x = copy.deepcopy(configuration)
+    x['fields'][0]['min_chars'] = -1
+    invalid_configurations.append(x)
+    # max_chars is greater than character limit
+    x = copy.deepcopy(configuration)
+    x['fields'][0]['max_chars'] = models.Length.C + 1
+    invalid_configurations.append(x)
+
+    return invalid_configurations
 
 
 def _build_invalid_email_configurations(configuration):
