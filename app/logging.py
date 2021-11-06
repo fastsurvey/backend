a0bec99a1c0ff13dc54964a5a19d2898cfd433fe
Felix Böhm <felix@felixboehm.dev>
2021-11-06 21:03:15 +0100

    Use loguru for logging and compress pydantic error details to a single line

--- app/logging.py
+++ app/logging.py
@@ -1,4 +1,23 @@
-import logging
+import loguru
+import sys
 
 
-LOGGER = logging.getLogger("uvicorn.error")
+LOGGER = loguru.logger
+LOGGER.remove()
+LOGGER.add(
+    sys.stderr,
+    colorize=True,
+    format=(
+        "<bold><dim>{time:YYYY-MM-DD HH:mm:ss.SS Z ddd}</></> | "
+        "<level>{level:8}</> | "
+        "<blue>{name}:{function}:{line}</> - {message}"
+    ),
+)
+
+
+def format_pydantic_error(exc):
+    """Format a pydantic validation error to a single line string for logging."""
+    errors = "["
+    for i, x in enumerate(str(exc).splitlines()[1:]):
+        errors += f"{x.strip()}, " if i % 2 else f"{x}: "
+    return errors[:-2] + "]"
